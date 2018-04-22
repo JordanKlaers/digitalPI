cd7cb8e804968c234e73488825be87bb6183e529
jordanklaers <jordan.klaers@gmail.com>
2018-04-22 12:22:42 -0600

    first

--- indexTwo.js
+++ indexTwo.js
@@ -11,13 +11,14 @@ board.on("ready", function() {
     var strip = new pixel.Strip({
         board: this,
         controller: "I2CBACKPACK",
-        strips: [{ pin: 6, length: 48 }] // 3 physical strips on pins 0, 1 & 2 with lengths 4, 6 & 8.
+        strips: [{ pin: 6, length: 144}] // 3 physical strips on pins 0, 1 & 2 with lengths 4, 6 & 8.
     });
 	console.log('strip?: ', strip);
     strip.on("ready", function() {
-        // do stuff with the strip here.
-		strip.pixel(0).color("rgb(0,5,0)");
-		
+		// do stuff with the strip here.
 		console.log('strip is on');
+		strip.pixel(0).color("rgb(0,5,0)");
+		console.log('strip sent color');
+		
     });
 });
