f7dcb31cf4d3dee1809c2611a7289c1d8d07366f
jordanklaers <jordan.klaers@gmail.com>
2018-04-21 22:51:04 -0600

    first

--- indexTwo.js
+++ indexTwo.js
@@ -1,13 +1,17 @@
-pixel = require("node-pixel");
-five = require("johnny-five");
+var pixel = require("node-pixel");
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
 
-var board = new five.Board(this);
-
+var board = new five.Board({
+  io: new Raspi()
+});
+console.log('board made');
 board.on("ready", function() {
+	console.log('board on');
     strip = new pixel.Strip({
         board: this,
         controller: "I2CBACKPACK",
-        strips: [4, 6, 8], // 3 physical strips on pins 0, 1 & 2 with lengths 4, 6 & 8.
+        strips: [144], // 3 physical strips on pins 0, 1 & 2 with lengths 4, 6 & 8.
     });
 
     strip.on("ready", function() {
