7d5823cbb6b6d377ffc94f5eff6f0ea9d4d8b951
jordanklaers <jordan.klaers@gmail.com>
2018-04-22 12:03:52 -0600

    first

--- indexTwo.js
+++ indexTwo.js
@@ -13,7 +13,7 @@ board.on("ready", function() {
         controller: "I2CBACKPACK",
         strips: [144], // 3 physical strips on pins 0, 1 & 2 with lengths 4, 6 & 8.
     });
-
+	console.log('strip?: ', strip);
     strip.on("ready", function() {
         // do stuff with the strip here.
 		strip.pixel(0).color("rgb(0,5,0)");
