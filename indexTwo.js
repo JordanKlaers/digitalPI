2b95e6787d59fc0d6d4f9b23340485395bbb77d2
jordanklaers <jordan.klaers@gmail.com>
2018-04-22 16:40:30 -0600

    first

--- indexTwo.js
+++ indexTwo.js
@@ -18,7 +18,9 @@ board.on("ready", function() {
 		// do stuff with the strip here.
 		console.log('strip is on');
 		strip.pixel(0).color("rgb(0,5,0)");
+		strip.show();
 		console.log('strip sent color');
+		
 		
     });
 });
